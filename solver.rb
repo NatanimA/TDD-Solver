8c016cc3b10dcdff7c583c67e83c07c30363100c
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-05 12:59:25 +0300

    Code Implementation for test one

--- solver.rb
+++ solver.rb
@@ -1,5 +1,11 @@
 class Solver
     def find_factorial(arg)
-
+      unless arg < 0
+        if arg == 0
+            1
+        else
+          arg.downto(1).reduce(:*)
+        end
+      end
     end
 end
