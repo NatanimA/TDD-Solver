687c227da9a397359fa05ab9360b95dc94916c45
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-05 13:02:04 +0300

    Test case for negative numbers

--- spec/_spec.rb
+++ spec/_spec.rb
@@ -9,5 +9,13 @@ describe Solver do
         it "It should return the factorial of the argument given" do
             expect(@solver.find_factorial(5)).to eql(120)
         end
+
+        it "It should return 1 if argument is 0" do
+            expect(@solver.find_factorial(0)).to eql(1)
+        end
+
+        it "It should raise argumentError when argument is negative integer" do
+            expect(@solver.find_factorial(-29)).to eql("Negative numbers not allowed!")
+        end
     end
 end
