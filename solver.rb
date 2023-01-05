8b23b3e6905c164b362646daeec6343d865f73ee
natanimabesha <natanimabesha@www.zhewutech.com>
2023-01-05 13:43:05 +0300

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,29 +1,29 @@
 class Solver
-    def find_factorial(arg)
-      unless arg.is_a? String
-        if arg == 0
-            1
-        elsif arg > 0
-          arg.downto(1).reduce(:*)
-        else
-          "Negative numbers not allowed!"
-        end
-      end
-    end
+  def find_factorial(arg)
+    return if arg.is_a? String
 
-    def reverse_string(arg)
-        arg.reverse
+    if arg.zero?
+      1
+    elsif arg.positive?
+      arg.downto(1).reduce(:*)
+    else
+      'Negative numbers not allowed!'
     end
+  end
 
-    def fizzbuzz(arg)
-      if arg % 3 == 0 && arg % 5 != 0
-        "fizz"
-      elsif arg % 3 != 0 && arg % 5 == 0
-        "buzz"
-      elsif arg % 3 == 0 && arg % 5 == 0
-        "fizzbuzz"
-      else
-        arg.to_s
-      end
+  def reverse_string(arg)
+    arg.reverse
+  end
+
+  def fizzbuzz(arg)
+    if (arg % 3).zero? && arg % 5 != 0
+      'fizz'
+    elsif arg % 3 != 0 && (arg % 5).zero?
+      'buzz'
+    elsif (arg % 3).zero? && (arg % 5).zero?
+      'fizzbuzz'
+    else
+      arg.to_s
     end
+  end
 end
